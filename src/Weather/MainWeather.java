9693ec1ee7159ced66ba75de12df572129cb3d8f
kroessle <krroessler81@gmail.com>
2024-05-03 12:10:32 -0500

    made the get weather class, need to make a weather object (reference chapter 4 flight) then a toString in the main class

--- src/Weather/MainWeather.java
+++ src/Weather/MainWeather.java
@@ -1,4 +1,14 @@
 package Weather;
 
+
+import java.util.Scanner;
+
 public class MainWeather {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a Zip code: ");
+        String zipCode = scan.nextLine();
+        System.out.println(WeatherController.getWeather(zipCode));
+
+    }
 }
--- src/Weather/WeatherController.java
+++ src/Weather/WeatherController.java
@@ -1,4 +1,83 @@
 package Weather;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 public class WeatherController {
+
+    public static String getWeather(String zipCode) {
+        float parsedZipCode = 0;
+        if (ZipValidator.parseZipCode(zipCode) != -1) {
+            parsedZipCode = ZipValidator.parseZipCode(zipCode);
+        } else {
+            System.out.println("Please Enter a Valid ZipCode");
+        }
+
+        JSONObject currentWeather = null;
+
+        try {
+
+            String urlStringLocation = String.format("https://api.geocod.io/v1.7/geocode?postal_code=%f&api_key=f167b9ffff7cca2721c97f96f1676cf2799f696&format=simple", parsedZipCode);
+
+            URL url = new URL(urlStringLocation);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
+            String inputLine;
+
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            JSONObject jsonResponse = new JSONObject(response.toString());
+
+            Double latitude = jsonResponse.getDouble("lat");// these are the output of the geocodio api
+            Double longitude = jsonResponse.getDouble("lng");
+
+
+            String urlStringWeather = String.format("https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&current_weather=true&temperature_unit=fahrenheit", latitude, longitude);
+
+            URL url2 = new URL(urlStringWeather);
+            HttpURLConnection con2 = (HttpURLConnection) url2.openConnection();
+            con2.setRequestMethod("GET");
+
+
+            BufferedReader in2 = new BufferedReader(new InputStreamReader(con2.getInputStream(), StandardCharsets.UTF_8));
+            String inputLine2;
+
+            StringBuilder response2 = new StringBuilder();
+
+            while ((inputLine2 = in2.readLine()) != null) {
+                response2.append(inputLine2);
+            }
+            in2.close();
+
+            // parse JSON response
+            JSONObject jsonResponse2 = new JSONObject(response2.toString());
+            currentWeather = jsonResponse2.getJSONObject("current_weather");
+
+            //calls to the api
+            double temperatureFahrenheit = currentWeather.getDouble("temperature");
+            int isDayOrNight = currentWeather.getInt("is_day");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return currentWeather.toString();
+    }
+
+//    @Override // incomplete make toString format a console response of the api's Data
+//    public String toString() {
+//        return "Weather\n" +
+//                "Temperature: ";
+//    }
 }
